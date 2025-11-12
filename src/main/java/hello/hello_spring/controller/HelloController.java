f80ee6c74c9e1b56e33d697ea548ac6ab5bc66ae
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-12 17:06:55 +0900

    docs: add comments explaining controller behavior

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") // '/hello' 경로로 들어오는 GET 요청
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
-        return "hello";
+        return "hello"; // 스프링 MVC의 ViewResolver가 resources/templates/hello.html 뷰 파일을 찾아 렌더링 지시
     }
 }
