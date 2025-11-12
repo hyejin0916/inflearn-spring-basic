4323cf0056a0c811862f51c37091392702c7a16b
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-12 17:36:14 +0900

    feat: section3-8 implement MVC with Thymeleaf templates

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,14 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello"; // 스프링 MVC의 ViewResolver가 resources/templates/hello.html 뷰 파일을 찾아 렌더링 지시
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        // 파라미터로 받아옴('?name=입력값'으로 조회), model에 담으면 view에서 렌더링할 때 씀
+        model.addAttribute("name", name);
+        // model → 컨트롤러에서 뷰로 데이터를 전달할 때 사용하는 객체
+        // addAttribute(key, value) → 데이터를 키-값 쌍으로 추가
+        return "hello-template";
     }
 }
